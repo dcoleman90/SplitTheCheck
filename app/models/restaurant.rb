776cc8399e99d522099a7577b52aa2eb0f0a929d
student <student@gowolves>
2017-03-25 12:40:59 -0400

    Final CSS for all pages complete. Make adjustments at end if necessary.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ApplicationRecord
 	def get_city_state_zip
-		city_state_zip = self.city + ", " 
-			+ self.state + " #{self.zip}"
+		city_state_zip = self.city + ", " + self.state + " #{self.zip}"
 	end
 end
