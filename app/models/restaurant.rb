d83556ab537e3c45c4c7a7c8089e6a8c576d8020
student <student@gowolves>
2017-03-26 14:30:56 -0400

    Final commit for sprint 1: Minor modifications made for Restaurant model verification. Also adjust notice and error css stylings.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -16,4 +16,12 @@ class Restaurant < ApplicationRecord
 		 return where("name LIKE ? OR address LIKE ? OR City LIKE ? OR state LIKE ? OR zip LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
 		end
 	end
+	
+	validates :name, :address, :city, :state, :zip, 
+		presence: true
+		
+	validates :state, length: { is: 2 }
+	
+	validates :zip,	numericality: { only_integer: true, 
+		greater_than_or_equal_to: 501 }
 end
