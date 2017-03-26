f27fcc3566f80b93f19a82cdeb07b9c032f39851
student <student@gowolves>
2017-03-26 09:43:10 -0400

    Testing complete for search feature

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -3,11 +3,29 @@ require 'test_helper'
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @restaurant = restaurants(:one)
+    @restaurants = Restaurant.all
   end
 
   test "should get index" do
     get restaurants_url
     assert_response :success
+  end
+  
+  test "should get searched result" do
+  	# total fixtures should equal to four at start
+  	assert_equal @restaurants.count, 4
+  	
+  	# after search they should account for two fixtures
+  	# because only two have chicago as a city
+  	assert_equal @restaurants.search("Chicago").count, 2
+  	
+  	# see if partial mixed case strings work for city as well
+  	assert_equal @restaurants.search("cHiCa").count, 2
+  	
+  	# make sure search by integer values works for full
+  	# and partial strings
+  	assert_equal @restaurants.search(60614).count, 1
+  	assert_equal @restaurants.search(60).count, 2
   end
 
   test "should get new" do
