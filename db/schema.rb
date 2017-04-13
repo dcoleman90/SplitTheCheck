d2ac2e2d2476d04c310a99e9b3ec533191b46cbf
student <student@gowolves>
2017-04-13 14:23:17 -0400

    Created users table, scaffolding, and functional tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323182300) do
+ActiveRecord::Schema.define(version: 20170413162331) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20170323182300) do
     t.integer  "down_votes", default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
