7e90e49218c571f3a4ab912d6ead672d305f5d72
student <student@gowolves>
2017-04-14 13:15:39 -0400

    Finished admin functionality and adjusted the navigation to make it easier for admin to adjust the users they have.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,12 @@ class SessionsController < ApplicationController
   # GET sessions/create
   def create
   	user = User.find_by(email: params[:email])
-  	if user.try(:authenticate, params[:password])
+	if user.try(:authenticate, params[:password]) && user.id == 4
+  		   session[:is_admin] = true
+  		   session[:user_id] = user.id	
+  		   redirect_to restaurants_url
+  	elsif user.try(:authenticate, params[:password])
+  		session[:is_admin] = false
   		session[:user_id] = user.id
   		redirect_to restaurants_url
   	else
@@ -20,6 +25,7 @@ class SessionsController < ApplicationController
   # GET sessions/destroy
   def destroy
   	session[:user_id] = nil
+  	session[:is_admin] = nil
   	redirect_to restaurants_url, notice: "Logged out"
   end
 end
